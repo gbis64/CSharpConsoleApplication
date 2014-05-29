629a565d91dc3643a931a349fd7a6927b322917d
gbis64 <gbis@sbcglobal.net>
2014-05-29 15:52:33 -0400

    C# Console Application
    C# Console Application

---
+++ CSharp Console Application/CSharp Console Application/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Console_Application
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i != 0)
+                {
+                    if (i % 3 == 0 && i != 15)
+                        Console.WriteLine("Modern");
+                    if (i % 5 == 0 && i != 15)
+                        Console.WriteLine("Medical");
+                    if (i % 3 == 0 && i % 5 == 0)
+                        Console.WriteLine("ModernMedical");
+                    if (i % 3 != 0 && i % 5 != 0)
+                        Console.WriteLine(i);
+                }
+            }
+            Console.Read();
+        }        
+    }
+}
